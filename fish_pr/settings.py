6f75bb0e5c3524940a5b943ba784d7ca70c0782c
nikifor511 <deadnikifor@gmail.com>
2020-04-22 22:58:47 +0300

    c

--- fish_pr/settings.py
+++ fish_pr/settings.py
@@ -142,9 +142,9 @@ LOGOUT_REDIRECT_URL  = 'fish_app:index'
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "fish_pr", "static")
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,  "static"),
+    os.path.join(BASE_DIR, "fish_pr", "static"),
 ]
 
 MEDIA_URL = '/media/'
