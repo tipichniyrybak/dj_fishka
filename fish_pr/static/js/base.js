0716fadcc7ef8167c6ba720c31ca9a02f73a7bd8
nikifor511 <deadnikifor@gmail.com>
2020-06-04 17:59:37 +0300

    12

--- fish_pr/static/js/base.js
+++ fish_pr/static/js/base.js
@@ -16,3 +16,56 @@ function closeForm(form) {
 
     }
 }
+
+let currUserID = 0;
+let currPlaceID = null;
+let currOrderID = null;
+let myYandexMap = null;
+let currProfile_json = "";
+
+$(document).ready(function() {
+
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+        $(this).toggleClass('active');
+    });
+
+    currUserID = '{{ request.user.id }}';
+
+
+    console.log('currUserID');
+    console.log(currUserID);
+    if (currUserID > 0) {
+
+        $.ajax({
+            type: 'POST',
+            url: '/get_profile_info',
+            data: {
+                'userID': currUserID
+            },
+            success: function(response) {
+                console.log('Profile_json:  ');
+                console.log(response);
+                currProfile_json = response;
+            } 
+
+        });
+
+        // $('#profile_controlsID').show();
+        // $('#login_formID').hide();
+        //reloadProfile();
+
+        lightbox.option({
+        'resizeDuration': 200,
+        'wrapAround': true
+        });
+
+
+    } else {
+        // $('#profile_controlsID').hide();
+        // $('#login_formID').show();
+    }
+
+    
+
+});
